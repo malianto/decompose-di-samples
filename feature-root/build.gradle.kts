7225449cd6b7daed0db5275e6ac1c1327b1f5f06
Anton Maliuta <anton.ktx@gmail.com>
2026-05-18 16:55:00 +0200

    Upgrade AGP to 9.2.1

--- feature-root/build.gradle.kts
+++ feature-root/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.android.library)
--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.android.library)
     alias(libs.plugins.metro)
 }
